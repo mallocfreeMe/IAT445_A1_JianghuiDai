551d00968ada0ccbaf8975082f55e148473dd6bd
Jianghui Dai <jianghuidai@outlook.com>
2021-01-27 22:21:59 -0800

    final update

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject airplane;
+    public void Play()
+    {
+        gameObject.SetActive(false);
+        player.SetActive(true);
+        airplane.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
