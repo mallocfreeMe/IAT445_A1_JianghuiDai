0e2b846b511cf1a3a651f8ffb7100bc4c5b3e3e5
Jianghui Dai <jianghuidai@outlook.com>
2021-02-10 23:41:57 -0800

    add the missing collision

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public ParticleSystem deathParticles;
+    
+    // if the cube collides with a fox
+    // then fox should be removed, and play its death sound and instantiate death particles 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fox"))
+        {
+            other.gameObject.GetComponent<AudioSource>().PlayOneShot(audioClip);
+            Instantiate(deathParticles, other.gameObject.transform.position, Quaternion.identity);
+            Destroy(other.gameObject, 1);
+        }
+    }
+}
